afd7d4f3b1d46fcf904a7e12535228bca20a885e
InSyn <insyner@gmail.com>
2023-06-20 13:02:11 +0700

    19/06/23 update

--- src/components/SectionEvent/SectionEvent.js
+++ src/components/SectionEvent/SectionEvent.js
@@ -12,6 +12,9 @@ const SectionEvent = observer(() => {
   const [activeEvents, setActiveEvents] = useState([]);
   const [activeDate, setActiveDate] = useState(new Date());
 
+  const [prevEvent, setPrevEvent] = useState();
+  const [nextEvent, setNextEvent] = useState();
+
   useEffect(() => {
     const TODAY_DATE = new Date().toDateString();
     // const TODAY_DATE_TIMESTAMP = Date.parse(new Date().toDateString());
@@ -26,6 +29,18 @@ const SectionEvent = observer(() => {
     // eslint-disable-next-line
   }, [setActiveEvents, appState.events]);
 
+  useEffect(()=>{
+    const prevEvents =  appState.events.filter(event => Date.parse(event.date) < Date.parse(activeDate.toDateString()))
+    if (prevEvents.length > 0) { setPrevEvent(prevEvents[prevEvents.length - 1]) }
+
+    const nextFoundEvent = appState.events.find(event =>
+        (Date.parse(event.date) > Date.parse(activeDate.toDateString())) &&
+        (new Date(event.date).toDateString() !== activeDate.toDateString())
+    )
+    if (nextFoundEvent) { setNextEvent(nextFoundEvent) }
+    // eslint-disable-next-line
+  }, [setPrevEvent, setNextEvent, activeDate, appState.events])
+
   return (
     <section className={styles.Section} id="events">
       <div className={styles.Left}>
@@ -37,26 +52,62 @@ const SectionEvent = observer(() => {
             >
               <h1 className={styles.Title}>СОБЫТИЯ</h1>
             </Link>
-            <div className={styles.LeftDate}>{formatDate(activeDate)}</div>
           </div>
 
           <div className={styles.Events}>
-            {activeEvents.length ? (
-              activeEvents.map((event) => (
-                <Link
-                  className={styles.Event}
-                  to={`/event/${event._id}`}
-                  key={event._id}
-                >
-                  {event.title}
-                  <ArrowIcon className={styles.arrowIcon}/>
-                </Link>
-              ))
-            ) : (
-              <p className={styles.EventsPlaceholder}>
-                В этот день событий нет
-              </p>
-            )}
+            {
+              prevEvent &&
+                <div className={styles.EventWrapper}>
+                  <div className={styles.EventDate}>
+                    {formatDate(prevEvent.date)}
+                  </div>
+                  <Link
+                      className={styles.Event}
+                      to={`/event/${prevEvent._id}`}
+                      key={prevEvent._id}
+                  >
+                    {prevEvent.title}
+                    <ArrowIcon className={styles.arrowIcon}/>
+                  </Link>
+                </div>
+            }
+
+            <div className={styles.EventWrapper}>
+              <div className={styles.LeftDate}>{formatDate(activeDate)}</div>
+              {activeEvents.length ? (
+                activeEvents.map((event) => (
+                  <Link
+                    className={styles.Event}
+                    to={`/event/${event._id}`}
+                    key={event._id}
+                  >
+                    {event.title}
+                    <ArrowIcon className={styles.arrowIcon}/>
+                  </Link>
+                ))
+              ) : (
+                <p className={styles.EventsPlaceholder}>
+                  В этот день событий нет
+                </p>
+              )}
+            </div>
+
+            {
+              nextEvent &&
+                <div className={styles.EventWrapper}>
+                  <div className={styles.EventDate}>
+                    {formatDate(nextEvent.date)}
+                  </div>
+                  <Link
+                      className={styles.Event}
+                      to={`/event/${nextEvent._id}`}
+                      key={nextEvent._id}
+                  >
+                    {nextEvent.title}
+                    <ArrowIcon className={styles.arrowIcon}/>
+                  </Link>
+                </div>
+            }
           </div>
         </div>
       </div>
--- src/components/StreamsPage/StreamsPage.jsx
+++ src/components/StreamsPage/StreamsPage.jsx
@@ -1,7 +1,14 @@
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./StreamsPage.module.css";
+import Header from "../Header/Header";
+import {useEffect} from "react";
+import {ReactComponent as ArrowIcon} from "./../../svg/arrow-icon.svg";
 
 const StreamsPage = () => {
+    useEffect(()=>{
+        window.scrollTo(0, 0);
+    })
+
   const navigate = useNavigate();
 
   const handleHomeLinkClick = (event) => {
@@ -11,17 +18,26 @@ const StreamsPage = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <Link
-        to="/"
-        className={styles.backLink}
-        onClick={handleHomeLinkClick}
-      >
-        На главную
-      </Link>
-      <iframe className={styles.frame} src="https://rtsp.me/embed/NenAsNKG/" title="первая трансляция"></iframe>
-      <iframe className={styles.frame} src="https://rtsp.me/embed/nDFs7nr3/" title="вторая трансляция"></iframe>
-    </div>
+    <>
+        <Header/>
+
+        <div className={styles.container}>
+            <Link to="/" className={styles.backLink} onClick={handleHomeLinkClick}>
+                <ArrowIcon />
+                На главную
+            </Link>
+
+            <div className={styles.cameraFrameWrapper}>
+                <h2>Камера на стадионе</h2>
+                <iframe className={styles.frame} src="https://rtsp.me/embed/NenAsNKG/" title="Камера на стадионе"></iframe>
+            </div>
+
+            <div className={styles.cameraFrameWrapper}>
+                <h2>Камера на лыжной базе</h2>
+                <iframe className={styles.frame} src="https://rtsp.me/embed/en2K9Q7d/" title="Камера на лыжной базе"></iframe>
+            </div>
+        </div>
+    </>
   );
 };
 
